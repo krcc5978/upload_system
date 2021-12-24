3d5de0a66e8c6e8484172ec7402f9ce52d888af4
t-asai <t-asai@inbility.co.jp>
2021-12-24 14:09:52 +0900

    1. コントローラとモデルの作成

---
+++ project/controller/upload_system_controller.py
@@ -0,0 +1,35 @@
+import time
+import threading
+from project.upload_vott import upload_vott
+
+
+def check_flag(model, button):
+    button['state'] = 'disable'
+    model.counter_text.set('アップロード準備中')
+
+    while model.flag.value:
+        if model.total.value == 0:
+            time.sleep(1 / 1000)
+        if model.total.value != 0:
+            model.counter_text.set(f'アップロード中　： ({str(model.count.value)} / {str(model.total.value)})')
+
+    model.counter_text.set(f'アップロード完了')
+    model.count.value = 0
+    model.flag.value = True
+    button['state'] = 'normal'
+
+
+def execute_upload(config, input_path, model, button):
+    upload_thread = threading.Thread(target=upload_vott,
+                                     args=(config,
+                                           input_path,
+                                           model.flag,
+                                           model.count,
+                                           model.total),
+                                     kwargs={'start_time': 10, 'skip_time': 10})
+    upload_thread.start()
+
+    check_thread = threading.Thread(target=check_flag,
+                                    args=(model,button,)
+                                    )
+    check_thread.start()
--- project/main.py
+++ project/main.py
@@ -1,21 +1,15 @@
-import threading
-import time
+
 import tkinter as tk
-from multiprocessing import Value
 from project.GUI_parts.init_parts import InitParts
-from project.upload_vott import upload_vott
+from project.controller.upload_system_controller import execute_upload
+from project.model.upload_system_model import UploadSystemModel
 
 
 class Application(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.flag = Value('b', True)
-        self.count = Value('i', 0)
-        self.total = Value('i', 0)
-
-        self.counter_text = tk.StringVar()
-        self.counter_text.set('')
+        self.ulm = UploadSystemModel()
 
         self.master.geometry("640x380")
         self.master.title('ファイルアップロード')
@@ -24,39 +18,16 @@ class Application(tk.Frame):
         self.config = InitParts(self.master, '設定ファイル', 3, 0, (10, 0), (10, 0))
         self.config.entry.insert(0, 'config.ini')
 
-        self.counter_label = tk.Label(self.master, textvariable=self.counter_text, font=('Helvetica', 10))
+        self.counter_label = tk.Label(self.master, textvariable=self.ulm.counter_text, font=('Helvetica', 10))
         self.counter_label.grid(row=5, column=1, sticky=tk.E, padx=(10, 0))
 
-        self.execute_button = tk.Button(self.master, text=u'実行', font=('Helvetica', 8), command=self.execute_upload)
+        self.execute_button = tk.Button(self.master, text=u'実行', font=('Helvetica', 8),
+                                        command=lambda: execute_upload(self.config.entry.get(),
+                                                                       self.input.entry.get(),
+                                                                       self.ulm,
+                                                                       self.execute_button
+                                                                       ))
         self.execute_button.grid(row=5, column=2, padx=(10, 0), sticky=tk.W)
-
-    def execute_upload(self):
-        upload_thread = threading.Thread(target=upload_vott,
-                                         args=(self.config.entry.get(),
-                                               self.input.entry.get(),
-                                               self.flag,
-                                               self.count,
-                                               self.total),
-                                         kwargs={'start_time': 10, 'skip_time': 10})
-        upload_thread.start()
-
-        check_thread = threading.Thread(target=self.check_flag)
-        check_thread.start()
-
-    def check_flag(self):
-        self.execute_button['state'] = 'disable'
-        self.counter_text.set('アップロード準備中')
-
-        while self.flag.value:
-            if self.total.value == 0:
-                time.sleep(1 / 1000)
-            if self.total.value != 0:
-                self.counter_text.set(f'アップロード中　： ({str(self.count.value)} / {str(self.total.value)})')
-
-        self.counter_text.set(f'アップロード完了')
-        self.count.value = 0
-        self.flag.value = True
-        self.execute_button['state'] = 'normal'
 
 
 def main():
